984797ac3fed30d9c8f3c8dc80491e92db32fe92
Freshhao <32076159+Freshhao@users.noreply.github.com>
2019-04-30 22:11:18 -0400

    Update server.js

--- back/server.js
+++ back/server.js
@@ -1,5 +1,5 @@
 const Koa = require("koa");
-const bodyParser = require("koa-bodyparser"); //中间件，
+const bodyParser = require("koa-bodyparser"); 
 const cors = require("koa2-cors");
 const session = require("koa-session");
 const router = require("./src/index")();
@@ -8,12 +8,12 @@ const path = require("path")
 
 const mongoose = require("mongoose");
 
-const db = "mongodb://127.0.0.1:27017/goods_db"; //mongoose 地址
+const db = "mongodb://127.0.0.1:27017/goods_db"; //mongoose address
 
-mongoose.connect(db, { useNewUrlParser: true }); //链接mongoose
+mongoose.connect(db, { useNewUrlParser: true }); //connect to mongoose
 
 const app = new Koa();
-// 配置静态web服务的中间件
+
 app.use(static(path.join(__dirname, './uploads')));
 app.keys = ["some secret hurr"];
 const CONFIG = {
@@ -27,7 +27,7 @@ const CONFIG = {
 };
 
 app.use(session(CONFIG, app));
-app.use(cors()); //跨域处理
+app.use(cors()); 
 app.use(bodyParser({ enableTypes: ["json", "form", "text"] }));
 app.use(router.routes()).use(router.allowedMethods());
 
